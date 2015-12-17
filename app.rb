0eb2b3461967adc9b3a81619cab92c73e5aa9910
Budi Sugianto <budisugianto777@gmail.com>
2015-12-17 17:55:37 -0500

    Fix the auth_generator

--- app.rb
+++ app.rb
@@ -21,6 +21,14 @@ end
 # the routes
 
 namespace '/api' do
+  def validate_user(request)
+    data = JSON.parse(request.body.read)
+    puts data
+    puts data['auth_token']
+    Users.check_token(data['auth_token'])
+    true
+  end
+
   post '/login' do
     data = JSON.parse(request.body.read)
     user = Users.authentication(data)
@@ -31,46 +39,54 @@ namespace '/api' do
     data = JSON.parse(request.body.read)
     user = Users.new(data)
     user.save
+    res = Users.send_email_registration(user)
+    return res.to_json
     status 201
-    user.to_json
   end
 
   get '/reset_password' do
     data = JSON.parse(request.body.read)
     user = Users.reset_password(data)
-
-  end
-
-  before(:each) do
-    data = JSON.parse(request.body.read)
-    Users.check_token(data[:auth_token])
+    Users.reset_password(user)
   end
 
   get '/users' do
-      Users.all.to_json
+    valid = validate_user(request)
+    if valid.eql? true
+      return Users.all.to_json
     end
+  end
 
   get '/users/:id' do
-    users = Users.find(params[:id])
-    return status 404 if Users.nil?
-    users.to_json
+    valid = validate_user(request)
+    if valid.eql? true
+      users = Users.find(params[:id])
+      return status 404 if Users.nil?
+      users.to_json
+    end
   end
 
 
   put '/users/:id' do
-    users = Users.find(params[:id])
-    return status 404 if users.nil?
-    data = JSON.parse(request.body.read)
-    users.update(data)
-    users.save
-    status 202
-    users.to_json
+    valid = validate_user(request)
+    if valid.eql? true
+      users = Users.find(params[:id])
+      return status 404 if users.nil?
+      data = JSON.parse(request.body.read)
+      users.update(data)
+      users.save
+      status 202
+      users.to_json
+    end
   end
 
   delete '/users/:id' do
-    users = Users.find(params[:id])
-    return status 404 if users.nil?
-    users.delete
-    status 202
+    valid = validate_user(request)
+    if valid.eql? true
+      users = Users.find(params[:id])
+      return status 404 if users.nil?
+      users.delete
+      status 202
+    end
   end
 end
--- model/users.rb
+++ model/users.rb
@@ -5,7 +5,7 @@ class Users
   include Mongoid::Timestamps
   include ActiveModel::MassAssignmentSecurity
 
-  attr_accessible :password
+  attr_accessible :password, :auth_token, :expired_time
   attr_protected :password_hash
 
   field :username, type: String
@@ -31,6 +31,7 @@ class Users
 
   def self.send_email(user, subject, body)
     # getting the mailtrap info and send it to pony mailer
+    ENV['MAILTRAP_API_TOKEN'] = 'a888304a1390fa4a818d7f9d66a1403a'
     response = RestClient.get "https://mailtrap.io/api/v1/inboxes.json?api_token=#{ENV['MAILTRAP_API_TOKEN']}"
     siji_inbox = JSON.parse(response)[0]
 
@@ -40,39 +41,59 @@ class Users
                                       :user_name      => siji_inbox['username'],
                                       :password       => siji_inbox['password'],
                                       :authentication => :plain,
-                                      :domain         => siji_inbox['domain']
-                    }}
+                                      :domain         => siji_inbox['domain'] }}
 
     Pony.mail({:to => user.email,
                 :subject => subject,
                 :body => body })
   end
 
+  def self.send_email_registration(user)
+    subject_email = 'Your Password has been reset'
+    body_email    = "Hi #{user.first_name}, \n
+                     Welcome to Siji \n
+                     Thank you! "
+
+    self.send_email(user, subject_email, body_email)
+    resposse = {message: 'Email Registration has been sent.'}
+    return resposse
+  end
+
+
   def self.reset_password(data)
     user = self.find_by(email: data['email'])
     random_pwd = SecureRandom.hex(7)
     user.password = random_pwd
     user.save
-  #   send email action here
+
+    subject_email = 'Your Password has been reset'
+    body_email    = "Hi #{user.first_name}, \n
+                     We recently received a request to reset your password \n
+                     Your new password is: #{random_pwd} \n
+                     Thank you ! "
+
+    self.send_email(user, subject_email, body_email)
   end
 
   def self.authentication(data)
     user = self.find_by(email: data['email'])
     if user.password == data['password']
-      # return 201
       token = generate_access_token
+      user.auth_token = token[:auth_token]
+      user.expired_time = token[:expired_time]
+      user.save
       return token
-      true
     else
-      # return 404
-      false
+      status 404
+      return false
     end
   end
 
   def self.generate_access_token
     auth_token   = SecureRandom.hex
     expired_time = Time.now + 3600 # token will expired within a hour
-    access_token = { auth_token: auth_token, expired_at: expired_time }
+    access_token = { auth_token: auth_token, expired_time: expired_time }
+
     return access_token
   end
 
